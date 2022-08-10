b0785e7f361ed0bead976fb3e53ef80bd61f13d9
Marlyn-Mayienga <marlymayienga@gmail.com>
2022-08-10 15:10:39 +0300

    created a todo list and styled it with an array of tasks

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
+
+
