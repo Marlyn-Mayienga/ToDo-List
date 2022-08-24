051ac763d3adfa6780f9f92b39465c18e0a65cbd
Marlyn-Mayienga <marlymayienga@gmail.com>
2022-08-24 19:43:00 +0300

    add test remove, add

--- src/task.test.js
+++ src/task.test.js
@@ -64,7 +64,56 @@ describe('TaskStore', () => {
     });
   });
   // Test addTask
-
+  describe('addTask', () => {
+    // add task to tasks
+    it('should add a task to the tasks array', () => {
+      const tasks = [{
+        index: 1,
+        title: 'Task 1',
+        description: 'Description 1',
+        completed: false,
+      }, {
+        index: 2,
+        title: 'Task 2',
+        description: 'Description 2',
+        completed: false,
+      }];
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      const newTask = {
+        index: 3,
+        title: 'Task 3',
+        description: 'Description 3',
+        completed: false,
+      };
+      TaskStore.addTask(newTask);
+      expect(TaskStore.getTasks()).toEqual([...tasks, newTask]);
+    });
+  });
   // Test removeTask
+  describe('removeTask', () => {
+    // remove task from tasks
+    it('should remove a task from the tasks array', () => {
+      const tasks = [{
+        index: 1,
+        title: 'Task 1',
+        description: 'Description 1',
+        completed: false,
+      }, {
+        index: 2,
+        title: 'Task 2',
+        description: 'Description 2',
+        completed: false,
+      }];
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      const newTask = [{
+        index: 1,
+        title: 'Task 1',
+        description: 'Description 1',
+        completed: false,
+      }];
+      TaskStore.removeTask(2);
+      expect(TaskStore.getTasks()).toEqual(newTask);
+    });
+  });
 
 });
